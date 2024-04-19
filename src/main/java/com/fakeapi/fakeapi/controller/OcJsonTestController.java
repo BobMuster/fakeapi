43ff66abbf27a1077b2b29b1aa43bfc8446f8612
Bob Mustaev <bobur.mustaev@becon.de>
2024-04-19 09:10:07 +0500

    Fixed bug where the function intended for sending single data items incorrectly sent a collection of data.

--- src/main/java/com/fakeapi/fakeapi/controller/OcJsonTestController.java
+++ src/main/java/com/fakeapi/fakeapi/controller/OcJsonTestController.java
@@ -1,16 +1,10 @@
 package com.fakeapi.fakeapi.controller;
 
 import com.fakeapi.fakeapi.service.UnitService;
-import com.fakeapi.fakeapi.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.File;
 
 @RestController
 @RequestMapping(value = "/api/json")
@@ -22,8 +16,8 @@ public class OcJsonTestController {
     private UnitService unitService;
 
     @GetMapping(value = "/user", produces = "application/json")
-    public ResponseEntity<?> getUser(@RequestParam(required = false) String name, @RequestParam("id") String id) throws Exception {
-        String user = unitService.getUserByNameAndId(id, name);
+    public ResponseEntity<?> getUser(@RequestParam("id") String id) throws Exception {
+        String user = unitService.getUserById(id);
         return ResponseEntity.ok(user);
     }
 
@@ -39,7 +33,7 @@ public class OcJsonTestController {
         return ResponseEntity.ok(units);
     }
 
-    @GetMapping(value = "/unit/{id}", produces = "application/json")
+    @GetMapping(value = "/unit/{unitId}", produces = "application/json")
     public ResponseEntity<?> getUnitById(@PathVariable String unitId) throws Exception {
         String unit = unitService.getUnitById(unitId);
         return ResponseEntity.ok(unit);
@@ -54,8 +48,7 @@ public class OcJsonTestController {
     @GetMapping(value = "/complex/unit", produces = "application/json")
     public ResponseEntity<String> getComplexUnit(@RequestBody MultiValueMap<String, String> valueMap) throws Exception {
         String unitId = valueMap.getFirst("unitID");
-        String networkName = valueMap.getFirst("networkName");
-        String complexUnit = unitService.getComplexUnitByNetworkNameAndId(unitId, networkName);
+        String complexUnit = unitService.getComplexUnitById(unitId);
         return ResponseEntity.ok(complexUnit);
     }
 
@@ -70,6 +63,4 @@ public class OcJsonTestController {
         String cUnits = unitService.getAllUnitComplex();
         return ResponseEntity.ok(cUnits);
     }
-
-
 }
--- src/main/java/com/fakeapi/fakeapi/service/UnitService.java
+++ src/main/java/com/fakeapi/fakeapi/service/UnitService.java
@@ -1,8 +1,5 @@
 package com.fakeapi.fakeapi.service;
 
-import com.fakeapi.fakeapi.dto.twitter.TwitterUser;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -10,7 +7,6 @@ import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 @Service
 public class UnitService {
@@ -28,29 +24,38 @@ public class UnitService {
     }
 
     public String getUnitById(String id)  {
-        String jsonPath = "$.[?(@.id == " + id + ")]";
-        return JsonPath.read(units, jsonPath).toString();
+        String jsonPath = "$.[?(@.unitID == '" + id + "')]";
+        String result = JsonPath.read(units, jsonPath).toString();
+        result = result.substring(1, result.length() - 1);
+        return result;
     }
 
     public String getUnitByNetworkName(String networkName) {
-        String jsonPath = "$.[?(@.networkName == " + networkName + ")]";
-        return JsonPath.read(units, jsonPath).toString();
+        String jsonPath = "$.[?(@.networkName == '" + networkName + "')]";
+        String result = JsonPath.read(units, jsonPath).toString();
+        result = result.substring(1, result.length() - 1);
+        return result;
     }
 
     public String getAllUnit() {
         return units;
     }
 
-    public String getComplexUnitByNetworkNameAndId(String id, String networkName) {
-        String jsonPath = "$.[?(@.networkName == '" + networkName + "' || @.unitID == '" + id + "')]";
-        return JsonPath.read(complexUnits, jsonPath).toString();
+    public String getComplexUnitById(String id) {
+        String jsonPath = "$.[?(@.unitID == '" + id + "')]";
+        String result = JsonPath.read(complexUnits, jsonPath).toString();
+        if (result.isEmpty()) {
+            return "";
+        }
+        result = result.substring(1, result.length() - 1);
+        return result;
     }
 
     public String getAllUnitComplex() {
         return complexUnits;
     }
 
-    public String getUserByNameAndId(String id, String name) {
+    public String getUserById(String id) {
         String jsonPath = "$.[?(@.id == '" + id + "')]";
         String result = JsonPath.read(users, jsonPath).toString();
         if (result.isEmpty()) {
